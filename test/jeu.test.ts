d129968f878e5a9a2b18d2cfea3aa9758b1a6e61
Cris Fuhrman <fuhrmanator+git@gmail.com>
2019-09-01 22:34:01 -0400

    Latest mocha, refactor tests

--- test/jeu.test.ts
+++ test/jeu.test.ts
@@ -9,18 +9,31 @@ const expect = chai.expect;
 let testNom1 = 'Jean-Marc';
 let testNom2 = 'Pierre';
 
+describe('GET /bo/gu/s/URL/', () => {
+
+  it('Call responds with bad request when bogus URL is sent.', () => {
+    return chai.request(app).get('/bo/gu/s/URL/' + testNom2)
+      .then(
+        response => {
+          expect(response.status).to.equal(404);
+        }
+      )
+  });
+});
+
+
 describe('GET /api/v1/jeu/demarrerJeu/:id', () => {
 
   it('responds with successful first call for player ' + testNom1, () => {
     return chai.request(app).get('/api/v1/jeu/demarrerJeu/' + testNom1)
-      .then(res => {
-        expect(res.status).to.equal(200);
-        expect(res).to.be.json;
-        expect(res.body.nom).to.equal(testNom1);
+      .then(response => {
+        expect(response.status).to.equal(200);
+        expect(response).to.be.json;
+        expect(response.body.nom).to.equal(testNom1);
       });
   });
 
-  it('duplicate call for player '  + testNom1 + ' responds with bad request', () => {
+  it('duplicate call for player ' + testNom1 + ' responds with bad request', () => {
     return chai.request(app).get('/api/v1/jeu/demarrerJeu/' + testNom1)
       .then(
         response => {
@@ -36,19 +49,19 @@ describe('GET /api/v1/jeu/demarrerJeu/:id', () => {
 describe('GET /api/v1/jeu/jouer/:id', () => {
 
   // plusieurs appels à jouer (pour valider la somme aléatoire)
-  for(let i=0; i < 20; i++) {
+  for (let i = 0; i < 20; i++) {
     it('responds with successful call for initialized player ' + testNom1, () => {
       return chai.request(app).get('/api/v1/jeu/jouer/' + testNom1)
-        .then(res => {
-          expect(res.status).to.equal(200);
-          expect(res).to.be.json;
-          expect(res.body.résultat.lancers).to.equal(i+1);
-          expect(res.body.résultat.v1).to.be.above(0).and.below(7);
-          expect(res.body.résultat.v2).to.be.above(0).and.below(7);
-          expect(res.body.résultat.somme).to.equal(res.body.résultat.v1 + res.body.résultat.v2);
-          expect(res.body.résultat.nom).to.equal(testNom1);
+        .then(response => {
+          expect(response.status).to.equal(200);
+          expect(response).to.be.json;
+          expect(response.body.résultat.lancers).to.equal(i + 1);
+          expect(response.body.résultat.v1).to.be.above(0).and.below(7);
+          expect(response.body.résultat.v2).to.be.above(0).and.below(7);
+          expect(response.body.résultat.somme).to.equal(response.body.résultat.v1 + response.body.résultat.v2);
+          expect(response.body.résultat.nom).to.equal(testNom1);
         });
-    });  
+    });
   }
 
   it('Call responds with bad request when player is not intialized ' + testNom2, () => {
@@ -68,12 +81,12 @@ describe('GET /api/v1/jeu/jouer/:id', () => {
 describe('GET /api/v1/jeu/terminerJeu/:id', () => {
   it('responds with successful call for player ' + testNom1, () => {
     return chai.request(app).get('/api/v1/jeu/terminerJeu/' + testNom1)
-      .then(res => {
-        expect(res.status).to.equal(200);
-        expect(res).to.be.json;
-        expect(res.body.résultat.nom).to.equal(testNom1);
+      .then(response => {
+        expect(response.status).to.equal(200);
+        expect(response).to.be.json;
+        expect(response.body.résultat.nom).to.equal(testNom1);
       });
-  }); 
+  });
 
   it('Call responds with bad request when player does not exist ' + testNom1, () => {
     return chai.request(app).get('/api/v1/jeu/terminerJeu/' + testNom1)
