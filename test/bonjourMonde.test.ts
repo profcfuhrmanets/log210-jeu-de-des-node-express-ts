b4eea288499e011ccf7af4e5ec1d8f2182ffbf1c
Cris Fuhrman <fuhrmanator+git@gmail.com>
2019-09-03 23:50:42 -0400

    remove needless query

--- test/bonjourMonde.test.ts
+++ test/bonjourMonde.test.ts
@@ -14,7 +14,7 @@ describe('baseRoute', () => {
   });
 
   it('should have the message in body', async () => {
-    const res = await chai.request(app).get('/').query({name: 'body'});
+    const res = await chai.request(app).get('/');
     expect(res.text).to.eql('<html><head><title>Hey</title></head><body><h1>Bonjour monde!</h1></body></html>');
   });
 
