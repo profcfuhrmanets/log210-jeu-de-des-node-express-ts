c776f28e73fa7c1f53ee2e5df642bb12bcaf0098
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2019-09-04 17:41:45 -0400

    move local css to dist

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const JSON_FILES = ['src/*.json', 'src/**/*.json'];
+const CSS_FILES = ['public/css/*.css'];
 
 // pull in the project TypeScript config
 const tsProject = ts.createProject('tsconfig.json');
@@ -15,9 +16,17 @@ gulp.task('watch', gulp.series('scripts', () => {
   gulp.watch('src/**/*.ts', ['scripts']);
 }));
 
-gulp.task('assets', function() {
+gulp.task('jsonAssets', function jsonAssets() {
   return gulp.src(JSON_FILES)
   .pipe(gulp.dest('dist'));
 });
 
+gulp.task('cssAssets', function cssAssets() {
+  return gulp.src(CSS_FILES, {base: './public'})
+  .pipe(gulp.dest('dist/public'));
+});
+
+gulp.task('assets', gulp.parallel('jsonAssets', 'cssAssets') );
+
 gulp.task('default', gulp.parallel('watch', 'assets'));
+
