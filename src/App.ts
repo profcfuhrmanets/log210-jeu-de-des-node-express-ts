c96252a1cc26b9b52c65889bebf17defd95511ce
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-07-09 12:29:00 -0400

    fix types

--- src/App.ts
+++ src/App.ts
@@ -1,7 +1,7 @@
-import * as express from 'express';
-import * as logger from 'morgan';
-import * as flash from 'node-twinkle';
-import * as ExpressSession from 'express-session';
+import express from 'express';
+import logger from 'morgan';
+import flash from 'node-twinkle';
+import ExpressSession from 'express-session';
 
 import { jeuRoutes } from './routes/JeuRouter';
 
@@ -17,15 +17,15 @@ class App {
     this.middleware();
     this.routes();
     this.expressApp.set('view engine', 'pug');
-    this.expressApp.use(express.static(__dirname + '/public')); // https://expressjs.com/en/starter/static-files.html
+    this.expressApp.use(express.static(__dirname + '/public') as express.RequestHandler); // https://expressjs.com/en/starter/static-files.html
 
   }
 
   // Configure Express middleware.
   private middleware(): void {
-    this.expressApp.use(logger('dev'));
-    this.expressApp.use(express.json());
-    this.expressApp.use(express.urlencoded({ extended: false }));
+    this.expressApp.use(logger('dev') as express.RequestHandler);
+    this.expressApp.use(express.json() as express.RequestHandler);
+    this.expressApp.use(express.urlencoded({ extended: false }) as express.RequestHandler);
     this.expressApp.use(ExpressSession(
       {
         secret: 'My Secret Key',
@@ -42,8 +42,6 @@ class App {
       let messages = res.locals.has_flashed_messages() ? res.locals.get_flashed_messages() : [];
       res.render('index', { title: 'Jeu de dés', flashedMessages: messages, joueurs: JSON.parse(jeuRoutes.jeu.getJoueurs()) });
     });
-
-
 
     this.expressApp.use('/', router);  // routage de base
 
