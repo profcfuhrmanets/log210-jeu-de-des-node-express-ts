a58bc6d15a17191a6098b3b19df87f1efb5b9ca7
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-08-25 13:50:48 -0400

    Meilleurs tests - exception inclue, messages avec devrait

--- test/core/Joueur.test.ts
+++ test/core/Joueur.test.ts
@@ -1,42 +1,54 @@
-import supertest from 'supertest';
 import 'jest-extended';
-
-import app from '../../src/App';
-const request = supertest(app);
-
+import { InvalidParameterError } from '../../src/core/errors/InvalidParameterError';
 import { Joueur } from '../../src/core/Joueur';
+
+let joueur1 = new Joueur('yvan');
 
 describe('Joueur test', () => {
 
-  it('joueur', () => {
-    let joueur: Joueur = new Joueur('yvan');
+  it('devrait initialiser un joueur avec un nom', () => {
+    const joueur = new Joueur('yvan');
     expect(joueur.nom).toEqual('yvan');
   });
 
-  it('assainir nom', () => {
-    let joueur: Joueur = new Joueur('yvan    ');
+  it('devrait lancer une exception si le nom est vide', () => {
+    const n = () => {
+      new Joueur('')
+    };
+    expect(n).toThrow(InvalidParameterError);
+    expect(n).toThrow("Le nom ne peut pas être vide")
+  })
+
+  it('devrait assainir un nom', () => {
+    const joueur = new Joueur('yvan    ');
     expect(joueur.nom).toEqual('yvan');
   });
 
-  it('lancer', () => {
-    let joueur: Joueur = new Joueur('yvan    ');
-    expect(joueur.lancers).toEqual(0);
-    joueur.lancer();
-    expect(joueur.lancers).toEqual(1);
+  it('devrait retourner 0 pour le nombre de lancers au début', () => {
+    expect(joueur1.lancers).toEqual(0);
   });
 
-  it('lancerGagner', () => {
-    let joueur: Joueur = new Joueur('yvan    ');
-    expect(joueur.lancersGagnes).toEqual(0);
-    joueur.gagner();
-    expect(joueur.lancersGagnes).toEqual(1);
+  it('devrait incrémenter le nombre de lancers', () => {
+    joueur1.lancer();
+    expect(joueur1.lancers).toEqual(1);
+    joueur1.lancer();
+    expect(joueur1.lancers).toEqual(2);
   });
 
-  it('lancerGagner', () => {
-    let joueur: Joueur = new Joueur('yvan    ');
-    expect(joueur.toJSON()).toEqual({"lancers": 0, "lancersGagnes": 0, "nom": "yvan"});
+  it('devrait retourner 0 pour le nombre de lancersGagnes au début', () => {
+    expect(joueur1.lancersGagnes).toEqual(0);
   });
 
-  
+  it('devrait incrémenter le nombre de lancersGagnes', () => {
+    joueur1.gagner();
+    expect(joueur1.lancersGagnes).toEqual(1);
+    joueur1.gagner();
+    expect(joueur1.lancersGagnes).toEqual(2);
+  });
+
+  it('devrait retourner un bon JSON', () => {
+    const joueur = new Joueur('yvan');
+    expect(joueur.toJSON()).toEqual({ "lancers": 0, "lancersGagnes": 0, "nom": "yvan" });
+  });
+
 });
- 
